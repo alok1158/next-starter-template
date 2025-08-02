1140972070f31155ff1d0f55036898e50bceeef2
Alok kumar <alokkumar1158@gmail.com>
2025-08-02 18:55:27 +0530

    some image related error fix config

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig = {
         pathname: '**',
       },
       {
-        protocol: 'http',
+        protocol: 'https',
         hostname: 'www.radiustheme.com',
         pathname: '**',
       },
