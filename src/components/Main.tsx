a22fe72e44ab7a49acfa319ec2c84c32ac11d136
Alok kumar <alokkumar1158@gmail.com>
2025-08-02 15:36:34 +0530

    test cloudflare add image

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,14 @@
+import Image from "next/image";
 import React from "react";
 
 const Main = () => {
   return (
     <div className="bg-lightImage bg-cover h-screen md:bg-cover md:bg-center">
-      <img
-        className="w-full min-h-screen"
-        src="http://99webpage.com/theme-review/landingpage/smooth/assets/images/masthead-img-work.png"
-        alt="/ "
-      />
+      <Image src={"http://99webpage.com/theme-review/landingpage/smooth/assets/images/masthead-img-work.png"}
+      alt="skjdks"
+      height={1000}
+      width={1000} 
+      className="w-full min-h-screen"/>
     </div>
   );
 };
